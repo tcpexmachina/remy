0309ad9c65fbc28c0e0224ebb11af3c1d2c98337
Keith Winstein <keithw@mit.edu>
2012-12-03 02:52:39 -0500

    Multiple senders

--- network-tester.cc
+++ network-tester.cc
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <vector>
 
 #include "dumbsender.hh"
 #include "network.hh"
 #include "receiver.hh"
 
+const int num_senders = 10;
+
 int main( void )
 {
-  DumbSender sender( 2, .1 );
+  std::vector< DumbSender > senders;
   Network net( .1 );
-  Receiver rec( 3 );
+  Receiver rec( num_senders );
+
+  for ( int i = 0; i < num_senders; i++ ) {
+    senders.emplace_back( i, 0.009 );
+  }
 
   for ( int i = 0; i < 20000000; i++ ) {
-    sender.tick( net, rec, i );
+    for ( auto &x : senders ) {
+      x.tick( net, rec, i );
+    }
+
     net.tick( rec, i );
   }
 
