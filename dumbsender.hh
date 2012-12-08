2f811790925b30485ac4a9c5c327fbe8351c53bd
Keith Winstein <keithw@mit.edu>
2012-12-08 04:42:00 -0500

    Kill switching

--- dumbsender.hh
+++ dumbsender.hh
@@ -9,14 +9,13 @@ class DumbSender
 private:
   unsigned int _id;
   int _packets_sent;
-  PoissonPointProcess _sending_process, _switching_process;
-  bool _active;
+  PoissonPointProcess _sending_process;
 
   /* stats */
   uint64_t total_packets, total_delay;
 
 public:
-  DumbSender( unsigned int s_id, const double s_rate, const double s_switch_rate );
+  DumbSender( unsigned int s_id, const double s_rate );
 
   void tick( Network & net, Receiver & rec, const int tickno );
 };
--- network-tester.cc
+++ network-tester.cc
@@ -5,23 +5,17 @@
 #include "network.hh"
 #include "receiver.hh"
 
-const int num_senders = 10;
+const int num_senders = 100;
 
 int main( void )
 {
-  std::vector< DumbSender > senders;
+
+  DumbSender sender( 0, 0.2 );
   Network net( .1 );
   Receiver rec( num_senders );
 
-  for ( int i = 0; i < num_senders; i++ ) {
-    senders.emplace_back( i, 0.013, .0000001 );
-  }
-
-  for ( int tick = 0; tick < 100000000; tick++ ) {
-    for ( auto &x : senders ) {
-      x.tick( net, rec, tick );
-    }
-
+  for ( int tick = 0; tick < 1000000000; tick++ ) {
+    sender.tick( net, rec, tick );
     net.tick( rec, tick );
   }
 
