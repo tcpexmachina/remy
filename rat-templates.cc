f9194b6cb7644031c1d91c1a4d8c75f44550bc84
Keith Winstein <keithw@mit.edu>
2013-01-22 18:11:51 -0500

    Fix semantics of intersend interval, although seems to hurt performance

--- rat-templates.cc
+++ rat-templates.cc
@@ -18,12 +18,13 @@ void Rat::send( const unsigned int id, NextHop & next, const unsigned int tickno
   const double wait_time( current_whisker.intersend() );
 
   while ( _packets_sent < _packets_received + window ) {
+    if ( _internal_tick > tickno + 1 ) {
+      return;
+    }
+
     Packet p( id, _packets_sent++, tickno );
     _memory.packet_sent( p );
     next.accept( move( p ) );
     _internal_tick += wait_time;
-    if ( _internal_tick >= tickno ) {
-      return;
-    }
   }
 }
