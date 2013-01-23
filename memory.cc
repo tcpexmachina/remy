92d32d81e3f054c62fef93a17f22d80236d51439
Keith Winstein <keithw@mit.edu>
2013-01-23 01:19:14 -0500

    Add RTT ratio

--- memory.cc
+++ memory.cc
@@ -1,4 +1,5 @@
 #include <vector>
+#include <assert.h>
 
 #include "memory.hh"
 
@@ -9,14 +10,20 @@ static const double alpha = 1.0 / 8.0;
 void Memory::packets_received( const vector< Packet > & packets )
 {
   for ( const auto &x : packets ) {
+    const unsigned int rtt = x.tick_received - x.tick_sent;
     if ( _last_tick_sent == 0 || _last_tick_received == 0 ) {
       _last_tick_sent = x.tick_sent;
       _last_tick_received = x.tick_received;
+      _min_rtt = rtt;
     } else {
       _rec_send_ewma = (1 - alpha) * _rec_send_ewma + alpha * (x.tick_sent - _last_tick_sent);
       _rec_rec_ewma = (1 - alpha) * _rec_rec_ewma + alpha * (x.tick_received - _last_tick_received);
       _last_tick_sent = x.tick_sent;
       _last_tick_received = x.tick_received;
+
+      _min_rtt = min( _min_rtt, rtt );
+      _rtt_ratio = double( rtt ) / double( _min_rtt );
+      assert( _rtt_ratio >= 1.0 );
     }
   }
 }
@@ -24,12 +31,12 @@ void Memory::packets_received( const vector< Packet > & packets )
 string Memory::str( void ) const
 {
   char tmp[ 64 ];
-  snprintf( tmp, 64, "sewma=%f, rewma=%f", _rec_send_ewma, _rec_rec_ewma );
+  snprintf( tmp, 64, "sewma=%f, rewma=%f, rttr=%f", _rec_send_ewma, _rec_rec_ewma, _rtt_ratio );
   return tmp;
 }
 
 const Memory & MAX_MEMORY( void )
 {
-  static const Memory max_memory( { 256, 256 } );
+  static const Memory max_memory( { 256, 256, 256 } );
   return max_memory;
 }
--- memory.hh
+++ memory.hh
@@ -13,31 +13,37 @@ public:
 private:
   DataType _rec_send_ewma;
   DataType _rec_rec_ewma;
+  DataType _rtt_ratio;
 
   unsigned int _last_tick_sent;
   unsigned int _last_tick_received;
+  unsigned int _min_rtt;
 
 public:
   Memory( const std::vector< DataType > & s_data )
     : _rec_send_ewma( s_data.at( 0 ) ),
       _rec_rec_ewma( s_data.at( 1 ) ),
+      _rtt_ratio( s_data.at( 2 ) ),
       _last_tick_sent( 0 ),
-      _last_tick_received( 0 )
+      _last_tick_received( 0 ),
+      _min_rtt( 0 )
   {}
 
   Memory()
     : _rec_send_ewma( 0 ),
       _rec_rec_ewma( 0 ),
+      _rtt_ratio( 0.0 ),
       _last_tick_sent( 0 ),
-      _last_tick_received( 0 )
+      _last_tick_received( 0 ),
+      _min_rtt( 0 )
   {}
 
-  void reset( void ) { _rec_send_ewma = _rec_rec_ewma = _last_tick_sent = _last_tick_received = 0; }
+  void reset( void ) { _rec_send_ewma = _rec_rec_ewma = _rtt_ratio = _last_tick_sent = _last_tick_received = _min_rtt = 0; }
 
-  static const unsigned int datasize = 2;
+  static const unsigned int datasize = 3;
 
-  const DataType & field( unsigned int num ) const { return num == 0 ? _rec_send_ewma : _rec_rec_ewma; }
-  DataType & mutable_field( unsigned int num ) { return num == 0 ? _rec_send_ewma : _rec_rec_ewma; }
+  const DataType & field( unsigned int num ) const { return num == 0 ? _rec_send_ewma : num == 1 ? _rec_rec_ewma : _rtt_ratio; }
+  DataType & mutable_field( unsigned int num ) { return  num == 0 ? _rec_send_ewma : num == 1 ? _rec_rec_ewma : _rtt_ratio; }
 
   void packet_sent( const Packet & packet __attribute((unused)) ) {}
   void packets_received( const std::vector< Packet > & packets );
@@ -45,9 +51,9 @@ public:
 
   std::string str( void ) const;
 
-  bool operator>=( const Memory & other ) const { return (_rec_send_ewma >= other._rec_send_ewma) && (_rec_rec_ewma >= other._rec_rec_ewma); }
-  bool operator<( const Memory & other ) const { return (_rec_send_ewma < other._rec_send_ewma) && (_rec_rec_ewma < other._rec_rec_ewma); }
-  bool operator==( const Memory & other ) const { return (_rec_send_ewma == other._rec_send_ewma) && (_rec_rec_ewma == _rec_rec_ewma); }
+  bool operator>=( const Memory & other ) const { return (_rec_send_ewma >= other._rec_send_ewma) && (_rec_rec_ewma >= other._rec_rec_ewma) && (_rtt_ratio >= other._rtt_ratio); }
+  bool operator<( const Memory & other ) const { return (_rec_send_ewma < other._rec_send_ewma) && (_rec_rec_ewma < other._rec_rec_ewma) && (_rtt_ratio < other._rtt_ratio); }
+  bool operator==( const Memory & other ) const { return (_rec_send_ewma == other._rec_send_ewma) && (_rec_rec_ewma == _rec_rec_ewma) && (_rtt_ratio == other._rtt_ratio); }
 };
 
 extern const Memory & MAX_MEMORY( void );
