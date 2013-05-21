ceb0d2f9fcb4dd4c1ecf021f34d30e27c6fb777d
Keith Winstein <keithw@ben.csail.mit.edu>
2013-05-21 09:33:41 -0400

    Optimize over factor-of-10 range of throughput (only)

--- rat-tester.cc
+++ rat-tester.cc
@@ -42,8 +42,8 @@ int main( int argc, char *argv[] )
   }
 
   Evaluator::ConfigRange configuration_range;
-  configuration_range.link_packets_per_ms = make_pair( 1, 2 ); /* 10 Mbps to 20 Mbps */
-  configuration_range.rtt_ms = make_pair( 100, 200 ); /* ms */
+  configuration_range.link_packets_per_ms = make_pair( .474, 4.74 ); /* 10 Mbps to 20 Mbps */
+  configuration_range.rtt_ms = make_pair( 150, 150 ); /* ms */
   configuration_range.max_senders = 2;
   //  configuration_range.lo_only = true;
   RatBreeder breeder( configuration_range );
