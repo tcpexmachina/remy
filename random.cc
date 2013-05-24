365977da0132e045527884817526b45c1b70262f
Keith Winstein <keithw@ben.csail.mit.edu>
2013-05-24 11:48:25 -0400

    Include PID in random seed

--- random.cc
+++ random.cc
@@ -1,9 +1,11 @@
 #include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "random.hh"
 
 PRNG & global_PRNG( void )
 {
-  static PRNG generator( time( NULL ) );
+  static PRNG generator( time( NULL ) ^ getpid() );
   return generator;
 }
