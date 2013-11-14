2c2ef5f2f3db9995d12b761e46e6cbe3b790dba6
Keith Winstein <keithw@mit.edu>
2013-11-14 17:37:16 -0500

    delay: add next_event_time

--- src/delay.hh
+++ src/delay.hh
@@ -24,8 +24,19 @@ public:
   void tick( NextHop & next, const double & tickno )
   {
     while ( (!_queue.empty()) && (std::get< 0 >( _queue.front() ) <= tickno) ) {
+      assert( std::get< 0 >( _queue.front() ) == tickno );
       next.accept( std::move( std::get< 1 >( _queue.front() ) ), tickno );
       _queue.pop();
+    }
+  }
+
+  double next_event_time( const double & tickno ) const
+  {
+    if ( _queue.empty() ) {
+      return std::numeric_limits<double>::max();
+    } else {
+      assert( tickno >= std::get< 0 >( _queue.front() ) );
+      return std::get< 0 >( _queue.front() );
     }
   }
 };
