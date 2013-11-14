f0a598cd1721bb6d589a874b0ef59fd718cc0e2e
Keith Winstein <keithw@mit.edu>
2013-11-14 16:10:35 -0500

    delay: go tickless

--- src/delay.hh
+++ src/delay.hh
@@ -9,19 +9,19 @@
 class Delay
 {
 private:
-  std::queue< std::tuple< unsigned int, Packet > > _queue;
-  const unsigned int _delay;
+  std::queue< std::tuple< double, Packet > > _queue;
+  const double _delay;
 
 public:
-  Delay( const unsigned int s_delay ) : _queue(), _delay( s_delay ) {}
+  Delay( const double s_delay ) : _queue(), _delay( s_delay ) {}
  
-  void accept( Packet && p, const unsigned int tickno ) noexcept
+  void accept( Packet && p, const double & tickno ) noexcept
   {
     _queue.emplace( tickno + _delay, std::move( p ) );
   }
 
   template <class NextHop>
-  void tick( NextHop & next, const unsigned int tickno )
+  void tick( NextHop & next, const double & tickno )
   {
     while ( (!_queue.empty()) && (std::get< 0 >( _queue.front() ) <= tickno) ) {
       next.accept( std::move( std::get< 1 >( _queue.front() ) ), tickno );
