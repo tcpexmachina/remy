1da73a50801847f87c5af6e8ea2f5b7dc26ef9f1
anirudhSK <sk.anirudh@gmail.com>
2013-11-16 12:14:03 -0500

    Take intersend time as input

--- src/dna-creator.cc
+++ src/dna-creator.cc
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-int main() {
-  const Whisker paced( 25600, 100, 0.5, MemoryRange( Memory(), MAX_MEMORY() ) );
+int main(int, const char* argv[]) {
+  const Whisker paced( 25600, 100, stod( argv[ 1 ] ) , MemoryRange( Memory(), MAX_MEMORY() ) );
   const WhiskerTree whiskers( paced, false );
   if ( !whiskers.DNA().SerializeToFileDescriptor( STDOUT_FILENO ) ) {
     fprintf( stderr, "Could not serialize whiskers.\n" );
