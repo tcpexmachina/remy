49e102902d1ce9da034bc59dd04f94b558ce91aa
Pratiksha Thaker <prthaker@mit.edu>
2013-12-06 00:10:43 -0500

    option to randomly initialize netconfig

--- src/network.hh
+++ src/network.hh
@@ -2,7 +2,10 @@
 #define NETWORK_HH
 
 #include <string>
+#include <boost/random/uniform_real_distribution.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
 
+#include "configrange.hh"
 #include "sendergang.hh"
 #include "link.hh"
 #include "delay.hh"
@@ -11,6 +14,10 @@
 
 class NetConfig
 {
+private:
+  bool is_random;
+  ConfigRange _range;
+
 public:
   double mean_on_duration, mean_off_duration;
   unsigned int num_senders;
@@ -18,12 +25,49 @@ public:
   double delay;
 
   NetConfig( void )
-    : mean_on_duration( 5000.0 ),
+    : is_random( false ),
+      _range(),
+      mean_on_duration( 5000.0 ),
       mean_off_duration( 5000.0 ),
       num_senders( 8 ),
       link_ppt( 1.0 ),
       delay( 150 )
   {}
+
+  /* 
+     If a configuration range is provided as an argument, 
+     draw the network's parameters uniformly at random from the 
+     configuration range whenever initialize() is called on this NetConfig.
+     Until initialize() is called, the parameters are set to the default
+     configuration.
+  */
+  NetConfig( ConfigRange & range )
+    : is_random( true ),
+      _range( range ),
+      mean_on_duration( 5000.0 ),
+      mean_off_duration( 5000.0 ),
+      num_senders( 8 ),
+      link_ppt( 1.0 ),
+      delay( 150 )
+  {}
+
+  void initialize()
+  {
+    if(!is_random) {
+      // Not to be randomly generated. Parameters should already be set.
+      return; 
+    }
+
+    boost::random::uniform_real_distribution<> link_speed( _range.link_packets_per_ms.first, _range.link_packets_per_ms.second );
+    boost::random::uniform_real_distribution<> rtt( _range.rtt_ms.first, _range.rtt_ms.second );
+    boost::random::uniform_int_distribution<> nsenders( 1, _range.max_senders );
+
+    link_ppt = link_speed(global_PRNG());
+    delay = rtt(global_PRNG());
+    num_senders = nsenders(global_PRNG());
+    mean_on_duration = _range.mean_on_duration;
+    mean_off_duration = _range.mean_off_duration;
+  }
 
   NetConfig set_link_ppt( const double s_link_ppt ) { link_ppt = s_link_ppt; return *this; }
   NetConfig set_delay( const double s_delay ) { delay = s_delay; return *this; }
