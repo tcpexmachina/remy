841ab6b6a626da299be68a834d67f71784d40b1a
Keith Winstein <keithw@mit.edu>
2013-12-16 16:42:46 -0500

    rat: Allow cap on flow size
    Conflicts:
    	src/rat-templates.cc

--- src/rat-templates.cc
+++ src/rat-templates.cc
@@ -6,7 +6,8 @@
 using namespace std;
 
 template <class NextHop>
-void Rat::send( const unsigned int id, NextHop & next, const double & tickno )
+void Rat::send( const unsigned int id, NextHop & next, const double & tickno,
+		const unsigned int packets_sent_cap )
 {
   assert( _packets_sent >= _largest_ack + 1 );
 
@@ -19,6 +20,12 @@ void Rat::send( const unsigned int id, NextHop & next, const double & tickno )
 
   if ( (_packets_sent < _largest_ack + 1 + _the_window)
        and (_last_send_time + _intersend_time <= tickno) ) {
+
+    /* Have we reached the end of the flow for now? */
+    if ( _packets_sent >= packets_sent_cap ) {
+      return;
+    }
+
     Packet p( id, _flow_id, tickno, _packets_sent );
     _packets_sent++;
     _memory.packet_sent( p );
