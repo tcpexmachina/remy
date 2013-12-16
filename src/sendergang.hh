06fc287aceb24fbd0c88ce031986d90adf2e74a5
Keith Winstein <keithw@mit.edu>
2013-12-16 17:27:14 -0500

    sendergang: Separate out methods for SwitchedSender vs. derived classes

--- src/sendergang.hh
+++ src/sendergang.hh
@@ -12,32 +12,38 @@ class SenderGang
 {
 private:
   class SwitchedSender {
-  public:
+  protected:
     unsigned int id;
     double next_switch_tick;
-    bool sending;
     SenderType sender;
-    Utility utility;
     double internal_tick;
-
-    SwitchedSender( const unsigned int s_id,
-		    const double & start_tick,
-		    const SenderType & s_sender )
-      : id( s_id ),
-	next_switch_tick( start_tick ),
-	sending( false ),
-	sender( s_sender ),
-	utility(),
-	internal_tick( 0 )
-    {}
-
-    double next_event_time( const double & tickno ) const;
 
     /* is abstract base class */
     virtual void switcher( const double & tickno,
 			   Exponential & start_distribution,
 			   Exponential & stop_distribution,
 			   const unsigned int num_sending ) = 0;
+
+    void switch_on( const double & tickno );
+    void switch_off( const double & tickno, const unsigned int num_sending );
+
+    void accumulate_sending_time_until( const double & tickno, const unsigned int num_sending );
+
+  public:
+    double next_event_time( const double & tickno ) const;
+    Utility utility;
+    bool sending;
+
+    SwitchedSender( const unsigned int s_id,
+		    const double & start_tick,
+		    const SenderType & s_sender )
+      : id( s_id ),
+	next_switch_tick( start_tick ),
+	sender( s_sender ),
+	internal_tick( 0 ),
+	utility(),
+	sending( false )
+    {}
   };
 
   class TimeSwitchedSender : public SwitchedSender {
