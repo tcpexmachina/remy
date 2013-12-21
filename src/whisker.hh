ad05f8a6cbedc4b4c1a74ffbc3640f02e91920d9
Keith Winstein <keithw@mit.edu>
2013-12-21 18:19:07 -0500

    whisker.hh: Disallow multiplicative increase of window

--- src/whisker.hh
+++ src/whisker.hh
@@ -106,7 +106,7 @@ public:
   static const OptimizationSettings & get_optimizer( void ) {
     static OptimizationSettings default_settings {
       { 0,            256,           1,  32, 4, 1 }, /* window increment */
-      { 0,            2,          0.01, 0.5, 4, 1 }, /* window multiple */
+      { 0,            1,          0.01, 0.5, 4, 1 }, /* window multiple */
       { 1.0 / 150.0,  30,  1.0 / 150.0,  30, 4, 15 } /* intersend */
     };
     return default_settings;
