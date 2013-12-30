34eff0170a0b175dce42e9b1ea6c8cb8695a410d
Pratiksha Thaker <prthaker@mit.edu>
2013-12-30 16:42:47 -0500

    updating script

--- src/batch_test.sh
+++ src/batch_test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+pls ./batch-runner nsrc=2 link=0.1 axes=0,1,2,3 of=signal_test_outfiles/test01
 pls ./batch-runner nsrc=2 link=0.1 axes=0,1,2 of=signal_test_outfiles/test01
+pls ./batch-runner nsrc=2 link=0.1 axes=0,1,3 of=signal_test_outfiles/test01
+pls ./batch-runner nsrc=2 link=0.1 axes=0,2,3 of=signal_test_outfiles/test01
+pls ./batch-runner nsrc=2 link=0.1 axes=1,2,3 of=signal_test_outfiles/test01
 pls ./batch-runner nsrc=2 link=0.1 axes=0,1 of=signal_test_outfiles/test01
+pls ./batch-runner nsrc=2 link=0.1 axes=0,3 of=signal_test_outfiles/test01
+pls ./batch-runner nsrc=2 link=0.1 axes=2,3 of=signal_test_outfiles/test01
 pls ./batch-runner nsrc=2 link=0.1 axes=0,2 of=signal_test_outfiles/test01
+pls ./batch-runner nsrc=2 link=0.1 axes=1,3 of=signal_test_outfiles/test01
 pls ./batch-runner nsrc=2 link=0.1 axes=1,2 of=signal_test_outfiles/test01
 pls ./batch-runner nsrc=2 link=0.1 axes=0 of=signal_test_outfiles/test01
-pls ./batch-runner nsrc=2 link=0.1 axes=2 of=signal_test_outfiles/test01
 pls ./batch-runner nsrc=2 link=0.1 axes=1 of=signal_test_outfiles/test01
+pls ./batch-runner nsrc=2 link=0.1 axes=2 of=signal_test_outfiles/test01
+pls ./batch-runner nsrc=2 link=0.1 axes=3 of=signal_test_outfiles/test01
