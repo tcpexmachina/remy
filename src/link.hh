91270f9ac7744df1a7cb99392cceff08838c6fb2
anirudhSK <sk.anirudh@gmail.com>
2013-12-30 21:15:56 -0500

    Add buffer size to Link, defaults to infinite buffer.

--- src/link.hh
+++ src/link.hh
@@ -13,14 +13,18 @@ private:
 
   Delay _pending_packet;
 
+  const std::queue< Packet >::size_type _limit;
+
 public:
-  Link( const double s_rate ) : _buffer(), _pending_packet( 1.0 / s_rate ) {}
+  Link( const double s_rate, const decltype( _limit ) s_limit = std::numeric_limits<decltype( _limit )>::max() ) : _buffer(), _pending_packet( 1.0 / s_rate ), _limit( s_limit ) {}
 
   void accept( Packet && p, const double & tickno ) noexcept {
     if ( _pending_packet.empty() ) {
       _pending_packet.accept( std::move( p ), tickno );
     } else {
-      _buffer.push( std::move( p ) );
+     if ( _buffer.size() < _limit ) {
+        _buffer.push( std::move( p ) );
+     }
     }
   }
 
