033a0ed9119d712405e58789e256d19f4fb975fd
Keith Winstein <keithw@mit.edu>
2014-03-03 07:45:37 +0000

    Rationalize correctly

--- src/fader.cc
+++ src/fader.cc
@@ -20,18 +20,14 @@ Fader::Fader( const string & filename )
     throw system_error( errno, system_category() );
   }
 
+  initialize();
+
+  auto output = physical_values_;
+
   for ( auto & x : physical_values_ ) {
     x = 255;
   }
 
-  auto output = physical_values_;
-  for ( auto & x : output ) {
-    x = 0;
-  }
-
-  initialize();
-
-  rationalize( output );
   write( output );
 }
 
@@ -40,6 +36,9 @@ void Fader::write( const decltype(physical_values_) & output )
   for ( unsigned int i = 0; i < physical_values_.size(); i++ ) {
     if ( output.at( i ) != physical_values_.at( i ) ) {
       array< uint8_t, 3 > write_buffer = { 176, uint8_t( i ), output.at( i ) };
+
+      cerr << "writing " << i << " = " << int( output.at( i ) ) << endl;
+
       ssize_t bytes_written = ::write( fd_, &write_buffer, write_buffer.size() );      
       if ( bytes_written != 3 ) {
 	throw runtime_error( "could not write to MIDI device" );
@@ -61,35 +60,35 @@ void Fader::compute_internal_state( void )
 
 void Fader::rationalize( decltype(physical_values_) & output ) const
 {
-  for ( uint8_t i = 0; i < 127; i++ ) {
+  for ( uint8_t i = 0; i < 128; i++ ) {
     if ( link_rate_ <= 0.316227766016838 * pow( 100.0, i / 127.0 ) ) {
       output.at( 81 ) = i;
       break;
     }
   }
 
-  for ( uint8_t i = 0; i < 127; i++ ) {
+  for ( uint8_t i = 0; i < 128; i++ ) {
     if ( time_increment_ <= (pow( 1.05, i ) - 1) / 100 ) {
       output.at( 88 ) = i;
       break;
     }
   }
 
-  for ( uint8_t i = 0; i < 127; i++ ) {
+  for ( uint8_t i = 0; i < 128; i++ ) {
     if ( horizontal_size_ <= pow( 1.07, i / 1.5 ) ) {
       output.at( 87 ) = i;
       break;
     }
   }
 
-  for ( uint8_t i = 0; i < 127; i++ ) {
+  for ( uint8_t i = 0; i < 128; i++ ) {
     if ( buffer_size_ <= (pow( 1.03, i ) - 1) * 500 ) {
       output.at( 83 ) = i;
       break;
     }
   }
 
-  for ( uint8_t i = 0; i < 127; i++ ) {
+  for ( uint8_t i = 0; i < 128; i++ ) {
     output.at( 89 ) = autoscale_ ? 127 : 0;
   }
 }
