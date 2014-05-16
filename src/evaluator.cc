a89093eb89ca054ab2ebe409b87462a4e5cd8a1c
anirudhSK <sk.anirudh@gmail.com>
2014-05-16 10:45:43 -0400

    1 or 2 senders.

--- src/evaluator.cc
+++ src/evaluator.cc
@@ -11,23 +11,14 @@ Evaluator::Evaluator( const WhiskerTree & s_whiskers, const ConfigRange & range 
     _whiskers( s_whiskers ),
     _configs()
 {
-  /* sample 100 link speeds */
-
-  const double steps = 100.0;
-
-  const double link_speed_dynamic_range = range.link_packets_per_ms.second / range.link_packets_per_ms.first;
-
-  const double multiplier = pow( link_speed_dynamic_range, 1.0 / steps );
-
   double link_speed = range.link_packets_per_ms.first;
 
-  /* this approach only varies link speed, so make sure no
-     uncertainty in rtt */
+  /* this approach only varies muxing */
   assert( range.rtt_ms.first == range.rtt_ms.second );
+  assert( range.link_packets_per_ms.first == range.link_packets_per_ms.second );
 
-  while ( link_speed <= (range.link_packets_per_ms.second * ( 1 + (multiplier-1) / 2 ) ) ) {
-    _configs.push_back( NetConfig().set_link_ppt( link_speed ).set_delay( range.rtt_ms.first ).set_num_senders( range.max_senders ).set_on_duration( range.mean_on_duration ).set_off_duration( range.mean_off_duration ) );
-    link_speed *= multiplier;
+  for ( uint32_t i = 1; i <= 2 ; i++ ) {
+    _configs.push_back( NetConfig().set_link_ppt( link_speed ).set_delay( range.rtt_ms.first ).set_num_senders( i ).set_on_duration( range.mean_on_duration ).set_off_duration( range.mean_off_duration ) );
   }
 }
 
--- src/remy.cc
+++ src/remy.cc
@@ -42,7 +42,7 @@ int main( int argc, char *argv[] )
   }
 
   ConfigRange configuration_range;
-  configuration_range.link_packets_per_ms = make_pair(2.2360679775, 4.472135955); /* 22.23.. Mbps to 44.721.. Mbps (2x range) */
+  configuration_range.link_packets_per_ms = make_pair( 1.5, 1.5 ); /* 15 Mbps */
   configuration_range.rtt_ms = make_pair( 150, 150 ); /* ms */
   configuration_range.max_senders = 2;
   configuration_range.mean_on_duration = 1000;
