9c6784335ac6479f18615d8a8170a58a5a4a41b1
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-30 01:56:02 -0700

    Add a version of send that doesn't need a NextHop template argument.

--- src/rat.cc
+++ src/rat.cc
@@ -42,6 +42,37 @@ void Rat::reset( const double & )
   assert( _flow_id != 0 );
 }
 
+std::vector< Packet > Rat::send( const unsigned int id,
+				 const double & tickno,
+				 const int packets_sent_cap )
+{
+  assert( _packets_sent >= _largest_ack + 1 );
+
+  if ( _the_window == 0 ) {
+    /* initial window and intersend time */
+    const Whisker & current_whisker( _whiskers.use_whisker( _memory, _track ) );
+    _the_window = current_whisker.window( _the_window );
+    _intersend_time = current_whisker.intersend();
+  }
+
+  if ( (_packets_sent < _largest_ack + 1 + _the_window)
+       and (_last_send_time + _intersend_time <= tickno) ) {
+
+    /* Have we reached the end of the flow for now? */
+    if ( _packets_sent >= packets_sent_cap ) {
+      return std::vector< Packet >();
+    }
+
+    Packet p( id, _flow_id, tickno, _packets_sent );
+    _packets_sent++;
+    _memory.packet_sent( p );
+    _last_send_time = tickno;
+    return std::vector< Packet >( 1, p );
+  } else {
+    return std::vector< Packet >();
+  }
+}
+
 double Rat::next_event_time( const double & tickno ) const
 {
   if ( _packets_sent < _largest_ack + 1 + _the_window ) {
