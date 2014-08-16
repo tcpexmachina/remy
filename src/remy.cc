4180b10043e8b7ff87d6a80ba4e287d201c406d3
anirudhSK <sk.anirudh@gmail.com>
2014-08-16 04:05:56 -0400

    Add 2x Remy.

--- src/remy.cc
+++ src/remy.cc
@@ -42,7 +42,7 @@ int main( int argc, char *argv[] )
   }
 
   ConfigRange configuration_range;
-  configuration_range.link_packets_per_ms = make_pair( 1.0, 10.0 ); /* 10 Mbps to 100 Mbps */
+  configuration_range.link_packets_per_ms = make_pair( 2.236, 4.472 ); /* 22 Mbps to 44 Mbps */
   configuration_range.rtt_ms = make_pair( 150, 150 ); /* ms */
   configuration_range.max_senders = 2;
   configuration_range.mean_on_duration = 1000;
