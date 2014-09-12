74be3ebc6e9f4f14d0a3ac2e4eb5266b3fc4fda4
Pratiksha Thaker <prthaker@mit.edu>
2014-09-12 12:00:19 -0400

    Changes to calculate rewma from reciever timestamp

--- src/memory.cc
+++ src/memory.cc
@@ -20,15 +20,15 @@ void Memory::packets_received( const vector< Packet > & packets, const unsigned 
     const double rtt = x.tick_received - x.tick_sent;
     if ( _last_tick_sent == 0 || _last_tick_received == 0 ) {
       _last_tick_sent = x.tick_sent;
-      _last_tick_received = x.tick_received;
+      _last_tick_received = x.time_at_receiver;
       _min_rtt = rtt;
     } else {
       _rec_send_ewma = (1 - alpha) * _rec_send_ewma + alpha * (x.tick_sent - _last_tick_sent);
-      _rec_rec_ewma = (1 - alpha) * _rec_rec_ewma + alpha * (x.tick_received - _last_tick_received);
-      _slow_rec_rec_ewma = (1 - slow_alpha) * _slow_rec_rec_ewma + slow_alpha * (x.tick_received - _last_tick_received);
+      _rec_rec_ewma = (1 - alpha) * _rec_rec_ewma + alpha * (x.time_at_receiver - _last_tick_received);
+      _slow_rec_rec_ewma = (1 - slow_alpha) * _slow_rec_rec_ewma + slow_alpha * (x.time_at_receiver - _last_tick_received);
 
       _last_tick_sent = x.tick_sent;
-      _last_tick_received = x.tick_received;
+      _last_tick_received = x.time_at_receiver;
 
       _min_rtt = min( _min_rtt, rtt );
       _rtt_ratio = double( rtt ) / double( _min_rtt );
--- src/whisker.hh
+++ src/whisker.hh
@@ -107,7 +107,7 @@ public:
     static OptimizationSettings default_settings {
       { 0,    256, 1,    32,  4, 1 }, /* window increment */
       { 0,    1,   0.01, 0.5, 4, 1 }, /* window multiple */
-      { 0.25, 3,   0.05, 1,   4, 3 } /* intersend */
+      { 0.25,    10,  0.05, 1,   4, 1.0 } /* intersend */
     };
     return default_settings;
   }
