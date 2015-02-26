c35f955c99f72a6d3139e37ed9dcb92a7a61c6c2
Pratiksha Thaker <prthaker@mit.edu>
2015-02-26 16:32:26 -0500

    Parallelize runs

---
+++ find-cycles.py
@@ -0,0 +1,15 @@
+import subprocess
+
+from multiprocessing import Pool
+
+rewma_range = [float(i) / 100 for i in range(0, 500, 10)]
+buffer_range = range(0, 200, 2)
+
+def build_cmd_and_call(args):
+    rewma, buff = args
+    call_str = "./src/find-cycles rewma=%f buff=%u if=dnafiles/absolute-intersend.dna.8" % (rewma, buff)
+    subprocess.call(call_str, shell=True)
+
+pool = Pool()
+pairs = [(r, b) for r in rewma_range for b in buffer_range]
+pool.map(build_cmd_and_call, pairs)
