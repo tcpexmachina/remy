d11e1ccd8285f88aae1c1c1b21a80fc5167b8af8
Chuan-Zheng Lee <czlee@stanford.edu>
2016-01-19 11:55:11 -0800

    Change default optimization settings

--- src/whisker.hh
+++ src/whisker.hh
@@ -105,9 +105,9 @@ public:
 
   static const OptimizationSettings & get_optimizer( void ) {
     static OptimizationSettings default_settings {
-      { 0,    256, 1,    32,  4, 1 }, /* window increment */
-      { 0,    1,   0.01, 0.5, 4, 1 }, /* window multiple */
-      { 0.25, 3,   0.05, 1,   4, 3 } /* intersend */
+      { 0        , 256, 1        , 32 , 4, 1 }, /* window increment */
+      { 0        , 1  , 0.01     , 0.5, 4, 1 }, /* window multiple */
+      { 2.0/300.0, 3  , 2.0/300.0, 30 , 4, 15 } /* intersend */
     };
     return default_settings;
   }
